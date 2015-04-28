b7fad6783097221e8aef3e0a7af1468dcb5033e0
hanulpark91 <hanul.parksf@gmail.com>
2015-04-28 16:53:24 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'sinatra/activerecord'
 require './environments'
 
 class Contact < ActiveRecord::Base
-  self.table_name = 'connect.contact'
+  self.table_name = 'salesforce.contact'
 end
 
 get "/contacts" do
